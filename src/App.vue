af3b7da8fd39beb52a67eafc1660869456e3a680
sharopcha <napster1202@gmail.com>
2021-06-02 07:02:19 +0300

    navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,61 @@
 <template>
   <div class="app-wrapper">
     <div class="app">
+      <Navigation />
       <router-view />
+      <Footer />
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "app",
-  components: {},
-  data() {
-    return {};
-  },
-  created() {},
-  mounted() {},
-  methods: {},
-  watch: {},
-};
+  import Navigation from './components/Navigation';
+  import Footer from './components/Footer';
+  export default {
+    name: 'app',
+    components: {
+      Navigation,
+      Footer,
+    },
+    data() {
+      return {};
+    },
+    created() {},
+    mounted() {},
+    methods: {},
+    watch: {},
+  };
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+  @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
 
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Quicksand", sans-serif;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Quicksand', sans-serif;
+  }
 
-.app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
 
-.container {
-  max-width: 1440px;
-  margin: 0 auto;
-}
+  .container {
+    max-width: 1440px;
+    margin: 0 auto;
+  }
 
-.link {
-  cursor: pointer;
-  text-decoration: none;
-  text-transform: uppercase;
-  color: black;
-}
+  .link {
+    cursor: pointer;
+    text-decoration: none;
+    text-transform: uppercase;
+    color: black;
+  }
 
-.link-light {
-  color: #fff;
-}
+  .link-light {
+    color: #fff;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-export default {
-  name: "Home",
-  components: {},
-};
+  export default {
+    name: 'Home',
+    components: {},
+    data() {
+      return {
+        welcomeScreen: {
+          title: 'Welcome!',
+          blogPost:
+            'Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!',
+          welcomeScreen: true,
+          photo: 'coding',
+        },
+      };
+    },
+  };
 </script>
