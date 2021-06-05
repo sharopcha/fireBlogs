b2b4a4702ce95e90fdfe6d7131df7d0405d6fe13
sharopcha <napster1202@gmail.com>
2021-06-05 06:42:04 +0300

    dynamic document titles

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,38 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Blogs from "../views/Blogs.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Blogs from '../views/Blogs.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
+    meta: {
+      title: 'Home',
+    },
   },
   {
-    path: "/blogs",
-    name: "Blogs",
+    path: '/blogs',
+    name: 'Blogs',
     component: Blogs,
+    meta: {
+      title: 'Blogs',
+    },
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | FireBlog`;
+  next();
 });
 
 export default router;
