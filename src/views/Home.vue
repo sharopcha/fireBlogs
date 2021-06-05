86295032019960b76263b63609b9c2f1d62ace40
sharopcha <napster1202@gmail.com>
2021-06-05 04:24:48 +0300

    register section in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,17 +18,27 @@
         </div>
       </div>
     </div>
+    <div class="updates">
+      <div class="container">
+        <h2>Never miss new posts</h2>
+        <router-link class="router-button" to="#">
+          Register to FireBlog <Arrow class="arrow arrow-light"></Arrow>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import BlogPost from '../components/BlogPost';
   import BlogCard from '../components/BlogCard';
+  import Arrow from '../assets/Icons/arrow-right-light.svg'
   export default {
     name: 'Home',
     components: {
       BlogPost,
       BlogCard,
+      Arrow
     },
     data() {
       return {
@@ -88,6 +98,44 @@
       font-weight: 300;
       font-size: 28px;
       margin-bottom: 32px;
+    }
+  }
+
+  .updates {
+    .container {
+      padding: 100px 25px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      
+      @media (min-width: 800px) {
+        padding: 125px 25px;
+        flex-direction: row;
+      }
+
+      .router-button {
+        display: flex;
+        font-size: 14px;
+        text-decoration: none;
+
+        @media (min-width: 800px) {
+          margin-left: auto;
+        }
+      }
+
+      h2 {
+        font-weight: 300;
+        font-size: 32px;
+        max-width: 425px;
+        width: 100%;
+        text-align: center;
+        text-transform: uppercase;
+
+        @media (min-width: 800) {
+          text-align: initial;
+          font-size: 40px;
+        }
+      }
     }
   }
 </style>
