3e0d89a70f7fc7a582796b289dbf49797fd4f48c
sharopcha <napster1202@gmail.com>
2021-06-20 01:33:52 +0300

    fixing blogCard and blogPost height issue

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -50,23 +50,21 @@
     cursor: pointer;
     display: flex;
     flex-direction: column;
+    border-radius: 8px;
     background-color: #fff;
-    min-height: 420px;
+    max-height: 420px;
     transition: 0.5s ease all;
-
     &:hover {
       transform: rotateZ(-1deg) scale(1.01);
-      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1) 0 2px 4px -1px
-        rgba(0, 0, 0, 0.06);
+      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+        0 2px 4px -1px rgba(0, 0, 0, 0.06);
     }
-
     .icons {
       display: flex;
       position: absolute;
       top: 10px;
       right: 10px;
       z-index: 99;
-
       .icon {
         display: flex;
         justify-content: center;
@@ -76,10 +74,8 @@
         border-radius: 50%;
         background-color: #fff;
         transition: 0.5s ease all;
-
         &:hover {
           background-color: #303030;
-
           .edit,
           .delete {
             path {
@@ -87,11 +83,9 @@
             }
           }
         }
-
         &:nth-child(1) {
           margin-right: 8px;
         }
-
         .edit,
         .delete {
           pointer-events: none;
@@ -108,7 +102,6 @@
       min-height: 200px;
       object-fit: cover;
     }
-
     .info {
       display: flex;
       flex-direction: column;
@@ -116,33 +109,28 @@
       z-index: 3;
       padding: 32px 16px;
       color: #000;
-
       h4 {
         padding-bottom: 8px;
         font-size: 20px;
         font-weight: 300;
       }
-
       h6 {
         font-weight: 400;
         font-size: 12px;
         padding-bottom: 16px;
       }
-
       .link {
         display: inline-flex;
         align-items: center;
         margin-top: auto;
         font-weight: 500;
         padding-top: 20px;
-        padding-bottom: 4px;
         font-size: 12px;
-        transition: 0.5s ease all;
-
+        padding-bottom: 4px;
+        transition: 0.5s ease-in all;
         &:hover {
           color: rgba(48, 48, 48, 0.8);
         }
-
         .arrow {
           width: 10px;
         }
